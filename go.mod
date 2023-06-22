072dc207aa441c117bd6a884c5c6cbbd1810f562
Fernando Mendoza <necrosora@gmail.com>
2023-06-22 16:00:48 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/FernandoMendoza12/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/FernandoMendoza12/godesde0/variables"
+)
+
+func main() {
+	var a int = 10
+	variables.MostrarEnteros(a)
+}
